92dfc7a07a2f5e403f4bde5c3c1f9f02277a8292
Alex Macrae <alex@mungorae.me.uk>
2021-01-27 15:46:07 +0000

    Project opened

---
+++ app/src/main/java/uk/me/mungorae/androidweekly/MainActivity.kt
@@ -0,0 +1,18 @@
+package uk.me.mungorae.androidweekly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uk.me.mungorae.androidweekly.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/uk/me/mungorae/androidweekly/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package uk.me.mungorae.androidweekly.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
